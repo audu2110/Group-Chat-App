89c01b8a82642504275153f426c8259ad3894485
Audumbar <audumbarak2000@gmail.com>
2023-03-14 16:21:47 +0530

    fifth

--- Group Chat App Frontend/chat/main.js
+++ Group Chat App Frontend/chat/main.js
@@ -37,32 +37,78 @@ const parseJwt = (token) => {
 };
 
 window.addEventListener("DOMContentLoaded", () =>{
-    const token  = localStorage.getItem('token')
-    const decodeToken = parseJwt(token)
-    console.log("decode tokenjhhvhjshshbshb",decodeToken.userId)
-    const uid=decodeToken.userId;
-    showAlltheUsers()
-    setInterval(axios.get("http://localhost:5000/message/getmessages", { headers: {"Authorization" : token} })
-    .then((response)=>{
-      console.log("all the data",response.data);
-      // console.log("all the data in database",response.data.allMessages.length);
-      // console.log("all the data in database",response.data.allMessages[0].Username);
+    // const token  = localStorage.getItem('token')
+    // const decodeToken = parseJwt(token)
+    // console.log("decode tokenjhhvhjshshbshb",decodeToken.userId)
+    // const uid=decodeToken.userId;
+    // showAlltheUsers()
+    // setInterval(axios.get("http://localhost:5000/message/getmessages", { headers: {"Authorization" : token} })
+    // .then((response)=>{
+    //   console.log("all the data",response.data);
+    //   // console.log("all the data in database",response.data.allMessages.length);
+    //   // console.log("all the data in database",response.data.allMessages[0].Username);
       
-      for(var i=0;i<response.data.allMessages.length;i++){
-        if(uid==response.data.allMessages[i].userId){
-          showNewUserOnScreen(response.data.allMessages[i],'outgoing');
-        }
-        else{
-          showNewUserOnScreen(response.data.allMessages[i],'incoming');
-        }
+    //   for(var i=0;i<response.data.allMessages.length;i++){
+    //     if(uid==response.data.allMessages[i].userId){
+    //       showNewUserOnScreen(response.data.allMessages[i],'outgoing');
+    //     }
+    //     else{
+    //       showNewUserOnScreen(response.data.allMessages[i],'incoming');
+    //     }
         
+    //   }
+    // })
+    // .catch((err)=>{
+    //   console.log(err);
+    // }), 1000)
+    
+    
+    const lsdata = JSON.parse(localStorage.getItem("messages"));
+
+  let lastid;
+  if (lsdata == null) {
+    lastid = 0;
+  } else {
+    lastid = lsdata[lsdata.length - 1].msgid;
+  }
+  let mergemsgs = [];
+  const token  = localStorage.getItem('token')
+  axios
+    .get(`http://localhost:5000/message/getmessages?id=${lastid}`, { headers: {"Authorization" : token} })
+    .then((messages) => {
+      if (messages.data.length > 0) {
+        if (lsdata) {
+          mergemsgs = lsdata.concat(messages.data);
+        } else {
+          mergemsgs = messages.data;
+        }
+        if (mergemsgs.lenght > 1000) {
+          let remove = mergemsgs.length - 1000;
+          for (let i = 0; i < remove; i++) {
+            mergemsgs.shift();
+          }
+        }
+      } else {
+        mergemsgs = JSON.parse(localStorage.getItem("messages"));
       }
-    })
-    .catch((err)=>{
-      console.log(err);
-    }), 1000)
-    
-    
+      localStorage.setItem("messages", JSON.stringify(mergemsgs));
+
+      const msgcontainer = document.getElementById("msgs");
+      msgcontainer.innerHTML = "";
+      msgcontainer.innerHTML = "<h1>Messages:</h1>";
+
+      for (let i = 0; i < mergemsgs.length; i++) {
+        const msgdiv = document.createElement("div");
+        msgdiv.classList.add("msgdiv");
+        const name = document.createElement("div");
+        name.innerHTML = `<p>${mergemsgs[i].Username}:</p>`;
+        msgdiv.appendChild(name);
+        const msg = document.createElement("div");
+        msg.innerHTML = `<p>${mergemsgs[i].message}</p>`;
+        msgdiv.appendChild(msg);
+        msgcontainer.appendChild(msgdiv);
+      }
+    });
   
   })
   
